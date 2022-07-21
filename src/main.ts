f07567842c9aa2e86f0465e231fab15b6e5fc9d6
Sakurai Kei <luqmanulraiserhakim@gmail.com>
2022-07-21 22:32:22 +0800

    Added tdl-stack.yml for docker stack deploy. The aws docker compose was used to deploy to aws but there was error in deployment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
@@ -17,15 +17,15 @@ async function bootstrap() {
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('ToDo List API')
-    .setDescription(
-      'User can create, update and delete tasks with API calls from client',
-    )
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('swag-api', app, document);
+  // const swaggerConfig = new DocumentBuilder()
+  //   .setTitle('ToDo List API')
+  //   .setDescription(
+  //     'User can create, update and delete tasks with API calls from client',
+  //   )
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, swaggerConfig);
+  // SwaggerModule.setup('swag-api', app, document);
 
   await app.listen(port, () => {
     console.log('[WEB]', config.get<string>('BASE_URL'));
