29abc35e5fcf17d8f8fa08b3f62cc3f964642713
Sakurai Kei <luqmanulraiserhakim@gmail.com>
2022-07-24 00:07:28 +0800

    Modified cors object

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: '*',
+      origin: 'http://localhost',
     },
   });
   const config: ConfigService = app.get(ConfigService);
